31218cd3232c459d4905a9a0ecac95a246fb1775
Mohamed-Yassine MADDOURI <m.y.maddouri@gmail.com>
2016-05-09 01:16:30 +0200

    Refactor package/g to use getopt

--- package/g/src/g.sh
+++ package/g/src/g.sh
@@ -1,8 +1,28 @@
 
-function g()
-{
+# if <path> is a directory, echo <path>
+# if <path> is a file (i.e. "-e <path>" is true), echo $(dirname <path>)
+# error otherwise
+function g_getdir() {  # ${FUNCNAME} <path>
+    if [ $# -ne 1 ] ; then
+        >&2 echo "${FUNCNAME}: expected 1 argument, received $# [$@]"
+        return 1
+    fi
+
+    local arg="$1"
+
+    if   [ -d "${arg}" ] ; then echo "${arg}"
+    elif [ -e "${arg}" ] ; then echo "$(dirname ${arg})"
+    else
+        >&2 echo "${FUNCNAME}: [${arg}] does not exist"
+        return 2
+    fi
+}
+
+# when the given path is a file not a directory, cd to that file's directory
+# otherwise, acts like cd
+function g() {
     local USAGE
-read -r -d '' USAGE << EndOfUsage
+    read -r -d '' USAGE << EndOfUsage
 Usage:
     g
     g [-L|-P] path
@@ -10,51 +30,68 @@ Usage:
     g -h|--help
 EndOfUsage
 
-    if [ "$#" -eq 0 ]                          # no arguments, go to the home directory
-    then
-        cd
-    elif [ "$#" -eq 1 ]                        # 1 argument: path or -
-    then
-        if [ "$1" = "-h" -o "$1" = "--help" ]  # help
-        then
-            cat "$(dirname ${BASH_SOURCE[0]})/../README.md"
-        elif [ "$1" = "-" ]                    # go to ${OLDPWD}
-        then
-            cd -
-        elif [ -d "$1" ]                       # path is a directory, cd to it
-        then
-            cd "$1"
-        elif [ -e "$1" ]                       # path is a file, cd to it's parent directory
-        then
-            cd "$(dirname $1)"
-        else
-            echo "Wrong path [$1]"
-            echo "${USAGE}"
-            return 1
-        fi
-    elif [ "$#" -eq 2 ]                    # 2 argument: -L|-P path
-    then
-        if [ "$1" = "-L" -o "$1" = "-P" ]  # -L|-P path
-        then
-            if [ -d "$2" ]                 # path is a directory, cd to it
-            then
-                cd "$1" "$2"
-            elif [ -e "$2" ]               # path is a file, cd to it's parent directory
-            then
-                cd "$1" "$(dirname $2)"
+    # http://www.bahmanm.com/blogs/command-line-options-how-to-parse-in-bash-using-getopt
+    # https://gist.github.com/cosimo/3760587
+    local OPTS
+    # https://stackoverflow.com/a/9525025/865719
+    OPTS=$(getopt -o 'L:P:h' -l 'help' -n "${FUNCNAME}" -- "$@")
+    if [ $? -ne 0 ] ; then >&2 echo "${USAGE}" ;  return 1 ; fi
+
+    # Set $1, $2... to the values of OPTS, even if it begins with '−' or '+':
+    # e.g. if $OPTS == "-a b --c d something"
+    # then
+    #    $1 == -a
+    #    $2 == b
+    #    $3 == --c
+    #    $4 == d
+    #    $5 == something
+    # which is useful for the "while" loop later and the calls to "shift"
+    eval set -- "${OPTS}"
+
+    # sanity check: $OPTS == $@
+    #echo "[OPTS:${OPTS}][#:$#][@:$@]"
+    # '--' is used at the beginning of the non-option arguments
+
+    local actual   # actual directory name -- after a call to g_getdir
+    case "$1" in
+        '-h'|'--help')
+            if [ $# -eq 2 -a "$2" = '--' ] ; then
+                cat "$(dirname ${BASH_SOURCE[0]})/../README.md"
             else
-                echo "Wrong path."
-                echo "${USAGE}"
+                >&2 echo "${USAGE}"
                 return 2
             fi
-        else
-            echo "Wrong first argument"
-            echo "${USAGE}"
-            return 3
-        fi
-    else
-        echo "Wrong argument count"
-        echo "${USAGE}"
-        return 4
-    fi
+        ;;
+        '-L'|'-P')
+            if [ $# -eq 3 -a "$3" = '--' ] ; then
+                actual=$(g_getdir "$2") ; if [ $? -ne 0 ] ; then >&2 echo "${USAGE}" ; return 4 ; fi
+                cd "$1" "${actual}"
+            else
+                >&2 echo "${USAGE}"
+                return 3
+            fi
+        ;;
+        '--')
+            shift  # get rid of '--'
+            #echo "[#:$#][@:$@]"
+            if [ $# -eq 0 ] ; then
+                cd
+            elif [ $# -eq 1 ] ; then
+                if [ "$1" = '-' ] ; then
+                    cd -
+                else
+                    actual=$(g_getdir "$1") ; if [ $? -ne 0 ] ; then >&2 echo "${USAGE}" ;  return 5 ; fi
+                    cd "${actual}"
+                fi
+            else
+                >&2 echo "${USAGE}"
+                return 6
+            fi
+        ;;
+        *)
+            >&2 echo "Unknown [${opt}]"
+            >&2 echo "${USAGE}"
+            return 7
+        ;;
+    esac
 }
